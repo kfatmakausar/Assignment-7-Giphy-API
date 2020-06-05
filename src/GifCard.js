11a00dcd46b42a0470edff68b9b9eae923cfbf50
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-04 20:16:55 -0400

    Initialized component files

---
+++ src/GifCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class GifCard extends Component {
+  render() {
+    return
+    
+  }
+}
+
+export default GifCard;
---
+++ src/SearchField.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class SearchField extends Component {
+  render() {
+    return 
+  }
+}
+
+export default SearchField;
